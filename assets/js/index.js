f320528184ac6f1e6e95733d66849facc9f50e79
nimajkrap <162951702+nimajkrap@users.noreply.github.com>
2025-07-09 10:17:02 +0900

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,3 @@
-// video
-const video = document.getElementById('myVideo');
-video.addEventListener('suspend', () => {
-});
-
-video.addEventListener('play', () => {
-});
-
-
 // SKill
 $(window).scroll(function () {
     const skillTop = $('#skill').offset().top;
